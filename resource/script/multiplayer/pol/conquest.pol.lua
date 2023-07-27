80c5d8436f4249641b03d18743ae62039eab083c
zverofaust <misanthrope099@hotmail.com>
2023-07-27 14:55:32 -0400

    Polish Jednostka Desantowa (Marines) breed files

---
+++ resource/script/multiplayer/pol/conquest.pol.lua
@@ -0,0 +1,25 @@
+Purchases["conquest.prc"] = {
+	{Repeat = 0, --infinite
+		Units = {
+			---[====[
+			-- Infantry
+				
+				-- Cannons	
+				--HMGs
+				--Anti Aircraft
+				--Anti Tank
+				--Mortars
+				--Infantry_Support
+				--Artillery
+				--Rocket Artillery
+				
+			--APCs/BTRs
+				
+			--Tanks
+				
+			--SPG
+				
+			--Air Assets
+		}
+	}
+}
